ec83af640b28f229d4553a4cafff4d1cc7af242b
Ruben Voß <bekertshop@gmail.com>
2023-03-25 23:54:55 +0100

    ..

--- app.py
+++ app.py
@@ -3,14 +3,11 @@ import sys
 
 payload = {'username': 'ruben', 'password': 25}
 try:
-    r = requests.post('https://httpbin.org/post', data=payload)
-# r = requests.get('https://htpbin.org/post', params=payload)
+    r = requests.get('https://httpbin.org/delay/3', auth=('ruben', 'pass'), timeout=2)
+    if r.ok:
+        print(f"Response is: {r.status_code} {r.reason}")
+        print(r)
+        print(r.json())
 except Exception as e:
     print(e)
     sys.exit(1)
-else:
-    print(r.url)
-    print(r.text)
-    print(r.json())
-# print(r.text)
-# print(help(r))
