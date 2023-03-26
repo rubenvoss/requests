b5a18f3297357c35ffd3b3e2d2ed58ce995c0c2f
Ruben Voß <bekertshop@gmail.com>
2023-03-26 15:25:56 +0200

    ..

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 #!/Users/ruben/code/requests/venv/bin/python
 # python3.11 -m venv venv --copies
+
+import xmltodict
 import requests
 import sys
 
 payload = {'username': 'ruben', 'password': 25}
 try:
-    r = requests.get('https://httpbin.org/delay/3', auth=('ruben', 'pass'), timeout=2)
+    r = requests.get('https://www.w3schools.com/xm/plant_catalog.xml', timeout=2)
     if r.ok:
         print(f"Response is: {r.status_code} {r.reason}")
-        print(r)
-        print(r.json())
+        # print(r)
+        # print(r.json())
+        dict = xmltodict.parse(r.text)
+        plants = dict['CATALOG']['PLANT']
+        for plant in plants:
+            print(plant)
+    else:
+        print(f"Response is: {r.status_code} {r.reason}")
 except Exception as e:
     print(e)
     sys.exit(1)
